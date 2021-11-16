63ff40404f0565da207235c3d8e0f2b30a90dfb9
Iti Tyagi <intechworx@gmail.com>
2021-11-16 11:08:16 +0100

    Create Pointer.cpp

---
+++ Pointer.cpp
@@ -0,0 +1,24 @@
+Question: https://www.hackerrank.com/challenges/c-tutorial-pointer/
+Solution:
+
+#include <stdio.h>
+#include <cstdlib>
+
+void update(int *a,int *b) {
+    // Complete this function  
+    int c = *a;  
+    *a = *a + *b;
+    *b = abs(c - *b);
+    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
